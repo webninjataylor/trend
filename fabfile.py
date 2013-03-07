f15ba838092799136b7ca10b68f9be2b7d78309f
Taylor Johnson <webninjataylor@gmail.com>
2013-03-07 16:45:41 -0500

    Added Fabric

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from __future__ import with_statement
+from fabric.api import local, settings, abort, run, cd, env, prefix
+from fabric.contrib.console import confirm
+
+
+env.hosts = ['webninj1@webninjataylor.com']
+
+
+def sync():
+    local("git status")
+    local("git add .")
+    local("git commit")
+    local("git push origin")
+
+
+def deploy():
+    code_dir = '/home6/webninj1/public_html/websites/trend/'
+
+    sync()
+
+    with cd(code_dir):
+        run("git pull")
